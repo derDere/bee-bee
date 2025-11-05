769a4ce7f6655cbe35ba4c2030807a08aa798b9c
Phillip Deremer <deremer@orbitlog.com>
2025-11-05 14:48:14 +0100

    Fix docker compose command order in start.sh
    Move the -p bee-bee project flag before the `up` subcommand so Docker Compose
    recognizes the project name option correctly.

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #cd bee-bee
 mkdir certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs\\key.pem -out certs\\cert.pem -subj "/CN=localhost"
-docker compose up -d -p bee-bee
+docker compose -p bee-bee up -d
