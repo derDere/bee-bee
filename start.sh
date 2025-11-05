79181e20762f1ba429fc61b67066f5d01c1e474f
derDere <phillip@deremer.de>
2025-11-05 19:53:35 +0100

    Run git pull in start.sh before building containers

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 mkdir certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs\\key.pem -out certs\\cert.pem -subj "/CN=localhost"
 docker compose -p bee-bee down
+git pull
 docker compose -p bee-bee build
 docker compose -p bee-bee up -d
